e31f9ad8b83fdbd20f6549da90fea4394c842a46
Orange4Broom <orang4broom@gmail.com>
2024-11-21 00:06:47 +0100

    Přidání fotek pro učebny a dat, úprava stylování pro otázky

--- src/components/elements/textBlock/TextBlock.tsx
+++ src/components/elements/textBlock/TextBlock.tsx
@@ -12,16 +12,18 @@ export const TextBlock: React.FC<TextBlockTypes> = ({
 }) => {
   return (
     <div className="textblock">
-      {showTitle === "howitworks" ? (
-        <h1 className="textblock__title">Jak naše apka funguje?</h1>
-      ) : showTitle === "goodchoise" ? (
-        <h1 className="textblock__title">Skvělá volba!</h1>
-      ) : showTitle === "classtitle" ? (
-        <h1 className="textblock__title">Jak zde probíhá výuka?</h1>
-      ) : showTitle === "correctanswers" ? (
-        <h1 className="textblock__title">Skvělá práce, vše máš správně!</h1>
-      ) : null}
-      <p className="textblock__description">{description}</p>
+      <div>
+        {showTitle === "howitworks" ? (
+          <h1 className="textblock__title">Jak naše apka funguje?</h1>
+        ) : showTitle === "goodchoise" ? (
+          <h1 className="textblock__title">Skvělá volba!</h1>
+        ) : showTitle === "classtitle" ? (
+          <h1 className="textblock__title">Jak zde probíhá výuka?</h1>
+        ) : showTitle === "correctanswers" ? (
+          <h1 className="textblock__title">Skvělá práce, vše máš správně!</h1>
+        ) : null}
+        <p className="textblock__description">{description}</p>
+      </div>
       {showTitle === "howitworks" && (
         <div>
           <p className="textblock__title">Rozmístění</p>
