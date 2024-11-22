2c6b25b12abef5424116b1ac893d438eab66fbd2
Orange4Broom <orang4broom@gmail.com>
2024-11-22 01:24:12 +0100

    Adding new demo production data

--- src/main.tsx
+++ src/main.tsx
@@ -4,32 +4,32 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { Home } from "./components/blocks/home/Home";
 import { Class } from "./components/elements/class/Class";
-import { MockUpData } from "./data/TestData";
+import { MockUpData } from "./data/Data";
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  const root = createRoot(rootElement);
+    const root = createRoot(rootElement);
 
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          {MockUpData.map((item) => (
-            <Route
-              key={item.id}
-              path={item.path}
-              element={
-                item.type === "main" ? (
-                  <Home item={item} />
-                ) : (
-                  <Class item={item} />
-                )
-              }
-            />
-          ))}
-        </Routes>
-      </Router>
-    </React.StrictMode>
-  );
+    root.render(
+        <React.StrictMode>
+            <Router>
+                <Routes>
+                    {MockUpData.map((item) => (
+                        <Route
+                            key={item.id}
+                            path={item.path}
+                            element={
+                                item.type === "main" ? (
+                                    <Home item={item} />
+                                ) : (
+                                    <Class item={item} />
+                                )
+                            }
+                        />
+                    ))}
+                </Routes>
+            </Router>
+        </React.StrictMode>
+    );
 }
